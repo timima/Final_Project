74c0b682fb6e7fc909299f2a9f7f03a8129c38f0
Timima Nasratshoeva <timima.nasratshoeva_2021@UCA.corp>
2020-12-20 21:04:28 +0000

    verifying token

--- utils/tokenParser.js
+++ utils/tokenParser.js
@@ -7,6 +7,13 @@ const checkIfLogged = (header) => {
         const token = authorization.split(" ")[1];
 
         //verifying token
+        if (jwt.verify(token, "password")){  //example password
+            return true; 
+        } else {
+            return false;
+        }
+    }
+    else return false;
     }
 }
 const getUserId = (header) => {
